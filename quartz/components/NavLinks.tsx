33061357d1b9d9a8a86546b952022862e1c5881c
otrossssss <pez.arroz.tabla@proton.me>
2026-05-03 19:54:29 -0300

    vault backup: 2026-05-03 19:54:29

--- quartz/components/NavLinks.tsx
+++ quartz/components/NavLinks.tsx
@@ -3,11 +3,10 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 const NavLinks: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
   return (
     <nav class={`links ${displayClass ?? ""}`}>
-      <a href="/el-jardín/index">🌿 Jardín</a>
-      <a href="/bitacora/index">📖 Bitácora</a>
+      <a href="/el-jardín">🌿 Jardín</a>
+      <a href="/notas">📖 Notas</a>
       <a href="/libro-de-visitas">📨 Visitas</a>
       <a href="/ahora">📅 Ahora</a>
-      <a href="/musica/index">🎧 Música</a>
       <a href="/links">🌐 Links</a>
     </nav>
   )
