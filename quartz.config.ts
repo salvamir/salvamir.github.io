b9a995f513901f9d71d3ce51635e8bb20f148080
otrossssss <pez.arroz.tabla@proton.me>
2026-07-16 23:23:28 -0300

    Cambio definitivo a salva.thedev.id

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "salvamir.github.io",
+    baseUrl: "salva.thedev.id",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
