561fbe2e2182a29921741901ddfd2af9135fe437
otrossssss <pez.arroz.tabla@proton.me>
2026-08-10 00:16:21 -0300

    vault backup: 2026-08-10 00:16:21

--- quartz/components/NavLinks.tsx
+++ quartz/components/NavLinks.tsx
@@ -1,11 +1,8 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 const NavLinks: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
-  return (
-    <nav class={`links ${displayClass ?? ""}`}>
-      <a href="https://salvamir.github.io/index.xml" target="_blank" rel="noopener noreferrer">🛜 RSS</a>
-      <a href="mailto:pez.arroz.tabla@proton.me">📩 Mail</a>
-    </nav>
-  )
+  // Retornamos null porque los botones ya están integrados en el HTML/Markdown
+  return null
 }
-export default (() => NavLinks) satisfies 
+
+export default (() => NavLinks) satisfies QuartzComponentConstructor
